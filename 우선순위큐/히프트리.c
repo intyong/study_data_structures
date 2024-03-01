82351a42a15c7f17e17ba00181a77db871ac3bb7
Suyong Jeong <wjdtndyd2001@gmail.com>
2024-03-01 23:50:31 +0900

    add some comment

--- 우선순위큐/히프트리.c
+++ 우선순위큐/히프트리.c
@@ -14,6 +14,11 @@ HeapType* create() {
 void init(HeapType* h) {
     h->heap_size = 0;
 }
+/* 새로운 값을 추가하는 경우, 가장 아래부터 값을 비교하며 올라옴
+   i = ++(h->heap_size)를 통해서 (item.key) 값을 가진 단말 노드로 시작
+   만약 새로 추가된 노드[i]가 부모노드[i / 2]보다 크다면, 새로 추가된 노드는 부모 노드의 값으로 변경
+   이후 i /= 2를 통해서 계속 노드를 타고 올라가면서 비교
+   적절한 위치를 찾으면 그 위치에 item 값을 넣어줌으로써 완료 */
 void insert_max_heap(HeapType* h, element item) {
     int i = 0;
     i = ++(h->heap_size);
@@ -23,6 +28,12 @@ void insert_max_heap(HeapType* h, element item) {
     }
     h->heap[i] = item;
 }
+/* 가장 우선 순위에 있는 노드의 값을 반환하는 함수, 가장 마지막에 있는 노드를 맨 위로 올린 후 쭉 내려옴
+   parent, child를 통해서 가장 앞 순위에 있는 노드의 값을 반환함과 동시에 히프가 흐트러지지 않도록 해줌
+   item은 반환하고자 하는 노드를 저장, temp에는 가장 마지막에 있는 노드를 저장함과 동시에 heap_size를 1만큼 줄여줌
+   while()을 통해서 왼쪽과 오른쪽 자식 노드의 key 값을 비교 후, 오른쪽 값이 더 크면 child++를 통해서 오른쪽 노드와 temp.key의 값을 비교
+   만약 temp.key의 값이 더 크면 그대로 그 위치에 temp가 저장되고, 작을 경우 1번 노드의 값에 (삭제된 값) 자식 노드의 값을 저장
+   이후 parent = child, child *= 2를 통해서 부모, 자식 관계를 유지하며 계속 내려감 */
 element delete_max_heap(HeapType* h) {
     int parent, child = 0;
     element item, temp;
@@ -30,6 +41,8 @@ element delete_max_heap(HeapType* h) {
     temp = h->heap[(h->heap_size)--];
     parent = 1;
     child = 2;
+    /* 오른쪽에 자식 노드가 있는지 확인 &&
+       왼쪽과 오른쪽 자식 노드의 key 값을 비교한 후, 만약 오른쪽 값이 더 크면 child++를 통해서 오른쪽 노드와 비교 */
     while (child <= h->heap_size) {
         if ((child < h->heap_size) && (h->heap[child].key) < h->heap[child + 1].key) child++;
         if (temp.key >= h->heap[child].key) break;
