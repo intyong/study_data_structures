71d3adcbe9155a4be538845ce486a63926baf797
Suyong Jeong <wjdtndyd2001@gmail.com>
2024-03-25 00:02:52 +0900

    how tired...

--- 정렬/merge_sort.c
+++ 정렬/merge_sort.c
@@ -11,26 +11,22 @@ void merge(int list[], int left, int mid, int right) {
     j = mid + 1;
     k = left;   // sorted[]를 위한 인덱스
     while (i <= mid && j <= right) {
-        if (list[i] <= list[j]) {
-            sorted[k++] = list[i++];
-        }
-        else {
-            sorted[k++] = list[j++];
-        }
+        if (list[i] <= list[j]) sorted[k++] = list[i++];
+        else sorted[k++] = list[j++];
     }
     if (i > mid) {
-        for (l = j; l <= right; l++) {
-            sorted[k++] = list[l];
-        }
+        for (l = j; l <= right; l++) sorted[k++] = list[l];
     }
     else {
-        for (l = i; l <= mid; l++) {
-            sorted[k++] = list[l];
-        }
+        for (l = i; l <= mid; l++) sorted[k++] = list[l];
     }
     for (l = left; l <= right; l++) {
         list[l] = sorted[l];
     }
+    for (int i = 0; i < MAX_SIZE; i++) {
+        printf("%d ", list[i]);
+    }
+    printf("\n");
 }
 void merge_sort(int list[], int left, int right) {
     int mid;
--- 정렬/quick_sort.c
+++ 정렬/quick_sort.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX_SIZE 9
+#define MAX_SIZE 5
 #define SWAP(x, y, t) ((t) = (x), (x) = (y), (y) = (t))
-int list[MAX_SIZE] = {5, 3, 8, 4, 9, 1, 6, 2, 7};
+int list[MAX_SIZE] = {2, 5, 1, 4, 3};
 int n;
 int partition(int list[], int left, int right) {
     int pivot, temp;
@@ -11,7 +11,7 @@ int partition(int list[], int left, int right) {
     high = right + 1;
     pivot = list[left];
     do {
-        /* low를 증가시키며 pivot보다 큰 값을 찾고, high를 증가시키며 pivot보다 작은 값을 찾는다
+        /* low를 증가시키며 pivot보다 큰 값을 찾고, high를 감소시키며 pivot보다 작은 값을 찾는다
            둘 다 찾은 경우 서로의 값을 교환, 이를 반복하여 정렬해줌 */
         do low++;
         while (list[low] < pivot && low < MAX_SIZE);
@@ -19,8 +19,12 @@ int partition(int list[], int left, int right) {
         while (list[high] > pivot && low >= 0);
         if (low < high) SWAP(list[low], list[high], temp);
     } while(low < high);
-    /* low, high의 값이 교차할 경우 반복문을 종료한 후 피봇을 적절한 위치로 변경 */
+    /* do, while()문의 반복을 통해 low와 high를 적절한 위치로 보낸 다음,
+       low, high의 값이 교차할 경우 반복문을 종료한 후 <피봇과 high가 가리키는 숫자와 위치를 변경> */
     SWAP(list[left], list[high], temp);
+    /* high의 위치를 반환해줌으로써 high 위치에 있는 수는 정렬된 수가 됨 */
+    for (int i = 0; i < MAX_SIZE; i++) printf("%d ", list[i]);
+    printf("\n");
     return high;
 }
 void quick_sort(int list[], int left, int right) {
@@ -36,7 +40,7 @@ int main() {
     for (int i = 0; i < n; i++) {
         printf("%d ", list[i]);
     }
-    printf("\nafter bubble sorting\n");
+    printf("\nafter quick sorting\n");
     quick_sort(list, 0, n - 1);
     for (int i = 0; i < MAX_SIZE; i++) {
         printf("%d ", list[i]);
