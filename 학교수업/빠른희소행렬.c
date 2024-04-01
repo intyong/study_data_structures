bdecbd2e6760193551685728f96ce7ab5e27b164
Suyong Jeong <wjdtndyd2001@gmail.com>
2024-04-02 00:17:12 +0900

    edit something to better understanding

--- 학교수업/빠른희소행렬.c
+++ 학교수업/빠른희소행렬.c
@@ -41,21 +41,15 @@ void fast_transpose(term a[], term b[]) {
         b[j].col = a[i].row;
         b[j].value = a[i].value;
         starting_pos[a[i].col]++;
-        printf("%d %d에 있던 거를 %d %d에 저장\n", a[i].row, a[i].col, b[j].row, b[j].col);
+        printf("%d %d에 있던 %d를 %d %d에 저장\n", a[i].row, a[i].col, a[i].value, b[j].row, b[j].col);
     }
 }
 int main() {
     term a[9] = {{6,6,8}, {0,0,15}, {0,3,22}, {0,5,-15}, {1,1,11}, {1,2,3}, {2,3,-6}, {4,0,91}, {5,2,28} };
     term b[9];
     printf("row: %d, col: %d, value: %d\n", a[0].row, a[0].col, a[0].value);
-    for (int i = 0; i < 9; i++) {
-        printf(" %d, %d, %d\n", a[i].row, a[i].col, a[i].value);
-    }
+
     fast_transpose(a,b);
-    printf("row: %d, col: %d, value: %d\n", b[0].row, b[0].col, b[0].value);
-    for (int i = 0; i < 9; i++) {
-        printf(" %d, %d, %d\n", b[i].row, b[i].col, b[i].value);
-    }
 
     return 0;
 }
