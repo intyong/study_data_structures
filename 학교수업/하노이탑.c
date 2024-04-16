d4d93aefd4bbcead670075c32a3fe9a62bda8186
Suyong Jeong <wjdtndyd2001@gmail.com>
2024-04-17 00:54:09 +0900

    add something

--- 학교수업/하노이탑.c
+++ 학교수업/하노이탑.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+/* hanoi_tower(n을 from에서 tmp를 거쳐 to로 옮긴다) */
 void hanoi_tower(int n, char from, char tmp, char to) {
     if (n == 1) printf("원판 1을 %c에서 %c로 옮긴다.\n", from, to);
     else {
