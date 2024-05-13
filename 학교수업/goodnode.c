98d82dc9cd67d4aac4d3b87768c7d3c5de7a0731
Suyong Jeong <wjdtndyd2001@gmail.com>
2024-05-14 00:03:41 +0900

    add something

---
+++ 학교수업/goodnode.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct TreeNode{
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+} TreeNode;
+int traversal(TreeNode* root, int prevMax) {
+    if (root == NULL) return 0;
+    int GoodNode = 0;
+    /* GoodNode인지 아닌지를 검사하는 거만 다름 */
+    if (root->val >= prevMax) {
+        GoodNode = 1;
+        prevMax = root->val;
+    }
+    int left = traversal(root->left, prevMax);
+    int right = traversal(root->right, prevMax);
+
+    return left + right + GoodNode;
+}
+int goodNodes(TreeNode* root) {
+    if (root == NULL) return 0;
+    int maxVal = root->val;
+    return traversal(root, maxVal);
+}
+int main() {
+    TreeNode n1 = {1, NULL, NULL};
+    TreeNode n4 = {4, NULL, NULL};
+    TreeNode n3 = {3, &n1, &n4};
+
+    int result = goodNodes(&n3);
+    printf("Number of good nodes: %d\n", result);
+
+    return 0;
+}
---
+++ 학교수업/isitBinaryTree.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+} TreeNode;
+int isBSTNode(TreeNode *root) {
+    if (root == NULL) return 1;
+    if ((root->left != NULL && root->val > root->left->val) && 
+        (root->right != NULL && root->val < root->right->val))
+        return 1;
+    return 0;
+}
+int traversal(TreeNode* root) {
+    if (root == NULL) return 1;
+    if (isBSTNode(root) == 0) return 0;
+    return traversal(root->left) && traversal(root->right);
+}
+int main() {
+    TreeNode n1 = {1, NULL, NULL};
+    TreeNode n4 = {4, NULL, NULL};
+    TreeNode n3 = {3, &n1, &n4};
+
+    int result = traversal(&n3);
+    if (result) printf("It is BST\n");
+    else printf("It isn't BST\n");
+
+    return 0;
+}
